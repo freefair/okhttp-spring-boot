e4813bd42ee331961a1fb49491a43d3ef13fbbf4
Lars Grefer <github@larsgrefer.de>
2017-01-01 14:56:24 +0100

    fix test project

--- test/src/main/java/io/freefair/spring/okhttp/test/OkHttpSpringBootStarterTestApplication.java
+++ test/src/main/java/io/freefair/spring/okhttp/test/OkHttpSpringBootStarterTestApplication.java
@@ -3,6 +3,9 @@ package io.freefair.spring.okhttp.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Lars Grefer
+ */
 @SpringBootApplication
 public class OkHttpSpringBootStarterTestApplication {
 
