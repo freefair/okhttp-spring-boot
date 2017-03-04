89137d7c8d42a19b7ed4d235ad642fd0ad85a617
Lars Grefer <github@larsgrefer.de>
2017-03-04 14:26:11 +0100

    add UnitTest

--- autoconfigure/src/test/java/io/freefair/spring/okhttp/OkHttp3AutoConfigurationTest.java
+++ autoconfigure/src/test/java/io/freefair/spring/okhttp/OkHttp3AutoConfigurationTest.java
@@ -1,8 +1,27 @@
-import static org.junit.Assert.*;
+package io.freefair.spring.okhttp;
+
+import okhttp3.OkHttpClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Created by larsgrefer on 04.03.17.
+ * @author Lars Grefer
  */
+@SpringBootTest(classes = OkHttp3AutoConfiguration.class)
+@RunWith(SpringRunner.class)
 public class OkHttp3AutoConfigurationTest {
+
+    @Autowired
+    OkHttpClient okHttpClient;
+
+    @Test
+    public void okHttp3Client() throws Exception {
+        assertThat(okHttpClient).isNotNull();
+    }
 
 }
