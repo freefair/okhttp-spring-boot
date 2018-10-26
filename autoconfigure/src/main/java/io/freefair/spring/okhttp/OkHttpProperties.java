ca87168faca0ffa347b6e4cf6d8761703a2a0f6c
Lars Grefer <github@larsgrefer.de>
2018-10-26 16:57:05 +0200

    remove unused imports

--- autoconfigure/src/main/java/io/freefair/spring/okhttp/OkHttpProperties.java
+++ autoconfigure/src/main/java/io/freefair/spring/okhttp/OkHttpProperties.java
@@ -1,7 +1,6 @@
 package io.freefair.spring.okhttp;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.util.unit.DataSize;
 
