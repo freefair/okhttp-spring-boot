0e53705fcc6539c6ce2af045b7e06de586d60ac6
Lars Grefer <github@larsgrefer.de>
2019-06-16 20:59:18 +0200

    Deprecate the Configurer interface

--- autoconfigure/src/main/java/io/freefair/spring/okhttp/Configurer.java
+++ autoconfigure/src/main/java/io/freefair/spring/okhttp/Configurer.java
@@ -2,8 +2,10 @@ package io.freefair.spring.okhttp;
 
 /**
  * @author Lars Grefer
+ * @deprecated Use {@link OkHttp3Configurer} instead.
  */
 @FunctionalInterface
+@Deprecated
 public interface Configurer<T> {
 
     void configure(T object);
