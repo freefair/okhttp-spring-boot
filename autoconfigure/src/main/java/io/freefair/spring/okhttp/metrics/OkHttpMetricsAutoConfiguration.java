bd6347ed87113a6200e181d33154154fd0a72159
Lars Grefer <github@larsgrefer.de>
2022-03-19 02:46:40 +0100

    Fix Metrics AutoConfiguration

--- autoconfigure/src/main/java/io/freefair/spring/okhttp/metrics/OkHttpMetricsAutoConfiguration.java
+++ autoconfigure/src/main/java/io/freefair/spring/okhttp/metrics/OkHttpMetricsAutoConfiguration.java
@@ -1,9 +1,13 @@
 package io.freefair.spring.okhttp.metrics;
 
+import io.freefair.spring.okhttp.OkHttpProperties;
+import io.micrometer.binder.okhttp3.OkHttpConnectionPoolMetrics;
+import io.micrometer.binder.okhttp3.OkHttpMetricsEventListener;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
-import io.micrometer.core.instrument.binder.okhttp3.OkHttpMetricsEventListener;
+import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -19,15 +23,18 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @AutoConfiguration(after = CompositeMeterRegistryAutoConfiguration.class)
-@ConditionalOnProperty(value = "okhttp.metrics.enabled", matchIfMissing = true, havingValue = "true")
 @ConditionalOnBean(MeterRegistry.class)
 @ConditionalOnClass({MeterRegistry.class, OkHttpMetricsEventListener.class, OkHttpClient.class})
 @EnableConfigurationProperties(OkHttpMetricsProperties.class)
 public class OkHttpMetricsAutoConfiguration {
 
+    @Autowired
+    private OkHttpMetricsProperties properties;
+
     @Bean
+    @ConditionalOnProperty(value = "okhttp.metrics.enabled", matchIfMissing = true, havingValue = "true")
     @ConditionalOnMissingBean
-    public OkHttpMetricsEventListener okHttpMetricsEventListener(MeterRegistry meterRegistry, OkHttpMetricsProperties properties) {
+    public OkHttpMetricsEventListener okHttpMetricsEventListener(MeterRegistry meterRegistry) {
         OkHttpMetricsEventListener.Builder builder = OkHttpMetricsEventListener
                 .builder(meterRegistry, properties.getName())
                 .includeHostTag(properties.isIncludeHostTag());
@@ -43,6 +50,19 @@ public class OkHttpMetricsAutoConfiguration {
         }
 
         return builder.build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "okhttp.metrics.pool.enabled", matchIfMissing = true, havingValue = "true")
+    @ConditionalOnBean(ConnectionPool.class)
+    @ConditionalOnMissingBean
+    public OkHttpConnectionPoolMetrics okHttpConnectionPoolMetrics(ConnectionPool connectionPool, OkHttpProperties okHttpProperties) {
+        OkHttpMetricsProperties.ConnectionPoolMetricsProperties poolProperties = properties.getPool();
+        return new OkHttpConnectionPoolMetrics(connectionPool,
+                poolProperties.getNamePrefix(),
+                getTags(poolProperties.getTags()),
+                okHttpProperties.getConnectionPool().getMaxIdleConnections()
+        );
     }
 
     private List<Tag> getTags(Map<String, String> tags) {
--- autoconfigure/src/main/java/io/freefair/spring/okhttp/metrics/OkHttpMetricsProperties.java
+++ autoconfigure/src/main/java/io/freefair/spring/okhttp/metrics/OkHttpMetricsProperties.java
@@ -1,9 +1,11 @@
 package io.freefair.spring.okhttp.metrics;
 
-import io.micrometer.core.instrument.binder.okhttp3.OkHttpMetricsEventListener;
+import io.micrometer.binder.okhttp3.OkHttpConnectionPoolMetrics;
+import io.micrometer.binder.okhttp3.OkHttpMetricsEventListener;
 import lombok.Data;
 import okhttp3.Request;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -44,4 +46,26 @@ public class OkHttpMetricsProperties {
      * @see OkHttpMetricsEventListener.Builder#tags(Iterable)
      */
     private Map<String, String> tags = new HashMap<>();
+
+    @NestedConfigurationProperty
+    private final ConnectionPoolMetricsProperties pool = new ConnectionPoolMetricsProperties();
+
+    /**
+     * @see OkHttpConnectionPoolMetrics
+     */
+    @Data
+    public static class ConnectionPoolMetricsProperties {
+
+        private boolean enabled = true;
+
+        /**
+         * @see OkHttpConnectionPoolMetrics#namePrefix
+         */
+        private String namePrefix = "okhttp.pool";
+
+        /**
+         * @see OkHttpConnectionPoolMetrics#tags
+         */
+        private Map<String, String> tags = new HashMap<>();
+    }
 }
