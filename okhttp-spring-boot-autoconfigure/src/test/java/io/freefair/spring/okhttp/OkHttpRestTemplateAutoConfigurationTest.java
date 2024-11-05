906b411a42ac134a7ba8908b91b94c06b01a54ba
Lars Grefer <github@larsgrefer.de>
2024-11-05 14:18:59 +0100

    polish

--- okhttp-spring-boot-autoconfigure/src/test/java/io/freefair/spring/okhttp/OkHttpRestTemplateAutoConfigurationTest.java
+++ okhttp-spring-boot-autoconfigure/src/test/java/io/freefair/spring/okhttp/OkHttpRestTemplateAutoConfigurationTest.java
@@ -12,7 +12,6 @@ import org.springframework.http.client.AbstractClientHttpRequestFactoryWrapper;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import java.lang.reflect.Field;
 import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,7 +28,7 @@ class OkHttpRestTemplateAutoConfigurationTest {
     private RestTemplateBuilder restTemplateBuilder;
 
     @Test
-    void testTimeouts() throws NoSuchFieldException, IllegalAccessException {
+    void testTimeouts() {
         RestTemplate restTemplate = restTemplateBuilder.setConnectTimeout(Duration.ofSeconds(42)).build();
 
         OkHttpClient client = extractClient(restTemplate);
@@ -39,13 +38,11 @@ class OkHttpRestTemplateAutoConfigurationTest {
         assertThat(client.writeTimeoutMillis()).isEqualTo(Duration.ofSeconds(21).toMillis());
     }
 
-    private OkHttpClient extractClient(RestTemplate restTemplate) throws NoSuchFieldException, IllegalAccessException {
+    private OkHttpClient extractClient(RestTemplate restTemplate) {
         ClientHttpRequestFactory requestFactory = restTemplate.getRequestFactory();
 
         while (requestFactory instanceof AbstractClientHttpRequestFactoryWrapper) {
-            Field field = AbstractClientHttpRequestFactoryWrapper.class.getDeclaredField("requestFactory");
-            field.setAccessible(true);
-            requestFactory = (ClientHttpRequestFactory) field.get(requestFactory);
+            requestFactory = ((AbstractClientHttpRequestFactoryWrapper) requestFactory).getDelegate();
         }
 
         assertThat(requestFactory).isInstanceOf(OkHttpClientRequestFactory.class);
