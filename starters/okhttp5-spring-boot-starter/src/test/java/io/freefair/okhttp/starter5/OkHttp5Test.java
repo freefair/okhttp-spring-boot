7af3610b1eeb748fbca3d1d8017545f9b57e99b3
Lars Grefer <github@larsgrefer.de>
2025-07-25 12:56:20 +0200

    Add tests

---
+++ starters/okhttp5-spring-boot-starter/src/test/java/io/freefair/okhttp/starter5/OkHttp5Test.java
@@ -0,0 +1,32 @@
+package io.freefair.okhttp.starter5;
+
+import okhttp3.OkHttpClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.assertj.AssertableApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class OkHttp5Test {
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    @Test
+    void contextLoads() {
+        assertThat(applicationContext).isNotNull();
+    }
+
+    @Test
+    void hasClientBean() {
+        AssertableApplicationContext context = AssertableApplicationContext.get(() -> applicationContext);
+
+        assertThat(context).hasSingleBean(OkHttpClient.class);
+        OkHttpClient okHttpClient = context.getBean(OkHttpClient.class);
+
+        assertThat(okHttpClient).isNotNull();
+    }
+}
