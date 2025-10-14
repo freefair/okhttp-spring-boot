b1c55b84e0623722f21f2d8609f389b07e6cc0f9
Lars Grefer <github@larsgrefer.de>
2025-10-15 00:19:14 +0200

    Fix tests

--- okhttp-spring-client/src/test/java/io/freefair/spring/okhttp/OkHttpResponseResourceTest.java
+++ okhttp-spring-client/src/test/java/io/freefair/spring/okhttp/OkHttpResponseResourceTest.java
@@ -1,11 +1,10 @@
 package io.freefair.spring.okhttp;
 
-import io.freefair.spring.okhttp.OkHttpResponseResource;
 import okhttp3.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.server.test.LocalServerPort;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ContentDisposition;
 
