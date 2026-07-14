a0bbfad84221d10e1551f52737739f8aa8c18b4e
Lars Grefer <github@larsgrefer.de>
2026-07-14 22:08:39 +0200

    Improve OkHttpClientRequestFactoryBuilder AutoConfiguration

--- okhttp-spring-boot-autoconfigure/src/test/java/io/freefair/spring/okhttp/OkHttpRestClientAutoConfigurationTest.java
+++ okhttp-spring-boot-autoconfigure/src/test/java/io/freefair/spring/okhttp/OkHttpRestClientAutoConfigurationTest.java
@@ -1,19 +1,16 @@
 package io.freefair.spring.okhttp;
 
-import io.freefair.spring.okhttp.client.OkHttpClientRequestFactory;
 import okhttp3.OkHttpClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.client.AbstractClientHttpRequestFactoryWrapper;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
-import java.lang.reflect.Field;
 import java.time.Duration;
 
+import static io.freefair.spring.okhttp.OkHttpTestUtils.extractClient;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
@@ -28,7 +25,7 @@ class OkHttpRestClientAutoConfigurationTest {
     private RestClient.Builder restClientBuilder;
 
     @Test
-    void testTimeouts() throws NoSuchFieldException, IllegalAccessException {
+    void testTimeouts() {
         RestClient restTemplate = restClientBuilder.build();
 
         OkHttpClient client = extractClient(restTemplate);
@@ -36,21 +33,6 @@ class OkHttpRestClientAutoConfigurationTest {
         assertThat(client.connectTimeoutMillis()).isEqualTo(Duration.ofSeconds(21).toMillis());
         assertThat(client.readTimeoutMillis()).isEqualTo(Duration.ofSeconds(21).toMillis());
         assertThat(client.writeTimeoutMillis()).isEqualTo(Duration.ofSeconds(21).toMillis());
-    }
-
-    private OkHttpClient extractClient(RestClient restClient) throws NoSuchFieldException, IllegalAccessException {
-
-        Field clientRequestFactoryField = restClient.getClass().getDeclaredField("clientRequestFactory");
-        clientRequestFactoryField.setAccessible(true);
-        ClientHttpRequestFactory requestFactory = (ClientHttpRequestFactory) clientRequestFactoryField.get(restClient);
-
-        while (requestFactory instanceof AbstractClientHttpRequestFactoryWrapper) {
-            requestFactory = ((AbstractClientHttpRequestFactoryWrapper) requestFactory).getDelegate();
-        }
-
-        assertThat(requestFactory).isInstanceOf(OkHttpClientRequestFactory.class);
-
-        return ((OkHttpClientRequestFactory)requestFactory).okHttpClient();
     }
 
     @SpringBootConfiguration
